60cd173c6ed11a22841947da03792247f7782441
Jeff Williams <jeffsmessages@gmail.com>
2013-10-21 16:22:12 -0400

    Updated createdb.rb script to output more info

--- bin/createdb.rb
+++ bin/createdb.rb
@@ -2,6 +2,8 @@
 require 'fileutils'
 require 'quartz_flow/model'
 
+DataMapper::Logger.new($stdout, :debug)
+
 $settings = {}
 def set(sym, value)
   puts "Set called: #{sym}=#{value}"
@@ -17,9 +19,12 @@ DataMapper.setup(:default, path)
 dir = File.dirname($settings[:db_file])
 FileUtils.mkdir dir if dir.length > 0 && ! File.directory?(dir)
 
+
 if ! File.exists?(dbPath)
+  puts "creating database #{path}"
   DataMapper.auto_migrate!
 else
+  puts "upgrading database #{path}"
   DataMapper.auto_upgrade!
 end
 
